b387903600e68507dedbc6cc01fba48e728bc996
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-12-28 11:34:46 +0530

    zssets index updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,7 +4,7 @@ import creative_production from './services/creative-production.png'
 import consulting from './services/consulting.png'
 // portfolio
 import portfolio1 from './portfolio/portfolio1.png'
-import portfolio2 from './portfolio/portfolio2.jpg'
+import portfolio2 from './portfolio/portfolio2.png'
 import portofolio3 from './portfolio/portfolio3.png'
 
 export {
