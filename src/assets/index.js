17e0e16c8f077e6bca6d88787443c51ba4702519
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-12-28 10:48:40 +0530

    updated assets

---
+++ src/assets/index.js
@@ -0,0 +1,18 @@
+import home from './images/home-image.png';
+import dig_market from './services/digital-marketing.png'
+import creative_production from './services/creative-production.png'
+import consulting from './services/consulting.png'
+// portfolio
+import portfolio1 from './portfolio/portfolio1.png'
+import portfolio2 from './portfolio/portfolio2.jpg'
+import portofolio3 from './portfolio/portfolio3.png'
+
+export {
+    home,
+    dig_market,
+    creative_production,
+    consulting,
+    portfolio1,
+    portfolio2,
+    portofolio3
+}
