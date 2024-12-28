85f71046a5926d83ec8dbeabc16368351aff482c
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-12-28 23:57:11 +0530

    font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        robot: "var(--font-robot)",
+      },
+    },
   },
   plugins: [],
 }
