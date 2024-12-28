caad5549d5dff69db3071dfce62bd22561a44295
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2024-12-28 10:49:01 +0530

    constants updated

---
+++ src/constants/index.js
@@ -0,0 +1,60 @@
+import { consulting, creative_production, dig_market, portfolio1, portfolio2, portofolio3 } from "../assets";
+
+export const stats = [
+  {
+    id: 1,
+    title: "500+",
+    subtitle: "Completed Projects",
+  },
+  {
+    id: 2,
+    title: "250+",
+    subtitle: "On Going Projects",
+  },
+  {
+    id: 3,
+    title: "300+",
+    subtitle: "Happy Clients",
+  },
+  {
+    id: 4,
+    title: "100+",
+    subtitle: "Professional Employee",
+  },
+];
+
+export const services = [
+    {
+        id: 1,
+        src: dig_market,
+        title: "Digital Marketing",
+    }, 
+    {
+        id: 2,
+        src: creative_production,
+        title: "Creative Productions",
+    }, 
+    {
+        id: 3,
+        src: consulting,
+        title: "Consulting",
+    }
+]
+
+export const portfolios = [
+    {
+        id: 1,
+        src: portfolio1,
+        title: "Portfolio 1",
+    }, 
+    {
+        id: 2,
+        src: portfolio2,
+        title: "Portfolio 2",
+    }, 
+    {
+        id: 3,
+        src: portofolio3,
+        title: "Portfolio 3",
+    }
+]
