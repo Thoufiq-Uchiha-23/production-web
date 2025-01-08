4a4b6e99d0de486f1b1408a313afb1c78ab90a86
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-09 00:39:31 +0530

    center aligned

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -4,7 +4,7 @@ import { portfolios, services, stats } from "../../constants";
 
 const Home = () => {
   return (
-    <div className="flex justify-center w-full h-auto mx-auto">
+    <div className="flex flex-col items-center justify-center w-full h-auto mx-auto">
       {/* HEADER SECTION */}
       <div
         className="text-center w-full flex justify-center items-center h-[600px] relative bg-cover bg-center"
@@ -27,35 +27,37 @@ const Home = () => {
         </div>
       </div>
       {/* ABOUT */}
-      <div className=" w-full flex justify-center mx-auto relative px-40 py-20 mt-[70px] gap-10">
-      <div className="flex items-center justify-evenly w-full mx-auto h-auto">
-        <div className="">
-          <h1 className="text-4xl font-light py-4">About Us</h1>
-          <p className="text-xl pb-7 leading-8 font-light">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed dapibus
-            placerat velit. Donec in porttitor elit. Suspendisse accumsan
-            iaculis tincidunt. placerat velit. Donec in porttitor elit.
-            Suspendisse accumsan iaculis tincidunt.{" "}
-          </p>
-          <button className="bg-yellow-500 px-5 py-2 rounded text-lg hover:shadow-md hover:bg-black hover:text-yellow-500 duration-500 transition-all ease-in-out">
-            Read More
-          </button>
-        </div>
+      <div className="flex items-center justify-center mx-auto relative px-40 py-20 mt-[70px] gap-10">
+        <div className="flex items-center justify-center h-auto">
+          <div className="w-full justify-evenly flex items-center">
+            <div className="w-[800px]">
+              <h1 className="text-4xl font-light py-4">About Us</h1>
+              <p className="text-xl pb-7 leading-8 font-light">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                dapibus placerat velit. Donec in porttitor elit. Suspendisse
+                accumsan iaculis tincidunt. placerat velit. Donec in porttitor
+                elit. Suspendisse accumsan iaculis tincidunt.{" "}
+              </p>
+              <button className="bg-yellow-500 px-5 py-2 rounded text-lg hover:shadow-md hover:bg-black hover:text-yellow-500 duration-500 transition-all ease-in-out">
+                Read More
+              </button>
+            </div>
 
-        <div className="w-full flex justify-center items-center">
-          <div className="w-[340px] h-auto flex items-center justify-between flex-wrap relative">
-            {stats.map((stat) => (
-              <div
-                key={stat.id}
-                className={`flex flex-col items-center relative justify-evenly rounded text-center w-[150px] h-auto bg-[#D9D9D92E]/18 border-[2px] border-[#0000001A] hover:border-none hover:shadow-md hover:bg-white duration-500 transition-all ease-in-out gap-2 p-4 even:mt-7`}
-              >
-                <h4 className="text-4xl font-light">{stat.title}</h4>
-                <p className="text-sm w-[80px]">{stat.subtitle}</p>
+            <div className="w-full flex justify-end items-center">
+              <div className="w-[340px] h-auto flex items-center justify-between flex-wrap relative">
+                {stats.map((stat) => (
+                  <div
+                    key={stat.id}
+                    className={`flex flex-col items-center relative justify-evenly rounded text-center w-[150px] h-auto bg-[#D9D9D92E]/18 border-[2px] border-[#0000001A] hover:border-none hover:shadow-md hover:bg-white duration-500 transition-all ease-in-out gap-2 p-4 even:mt-7`}
+                  >
+                    <h4 className="text-4xl font-light">{stat.title}</h4>
+                    <p className="text-sm w-[80px]">{stat.subtitle}</p>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
           </div>
         </div>
-      </div>
       </div>
       {/* SERVICES */}
       <div className="flex justify-center flex-col items-center w-full h-auto px-40 py-20">
@@ -87,7 +89,11 @@ const Home = () => {
                 key={portfolio.id}
                 className={`flex flex-col border items-center relative justify-evenly hover:shadow-lg overflow-hidden hover:scale-125 duration-500 transition-all ease-in-out gap-2 rounded-[10px]`}
               >
-                <img src={portfolio.src} alt={portfolio.title} className="object-fit w-[300px] h-[200px] object-center object-cover"/>
+                <img
+                  src={portfolio.src}
+                  alt={portfolio.title}
+                  className="object-fit w-[300px] h-[200px] object-center object-cover"
+                />
               </div>
             ))}
           </div>
