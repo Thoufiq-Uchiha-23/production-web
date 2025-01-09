6ff5f204d0d68274e2e4624c0b553434f5f5c069
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-09 16:38:41 +0530

    home page updated

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,34 +1,47 @@
 import React from "react";
-import { home } from "../../assets";
+import { designdot, home } from "../../assets";
 import { portfolios, services, stats } from "../../constants";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center w-full h-auto mx-auto">
+    <div className="flex flex-col items-center justify-center w-full h-auto mx-auto overflow-x-hidden">
       {/* HEADER SECTION */}
       <div
-        className="text-center w-full flex justify-center items-center h-[600px] relative bg-cover bg-center"
+        className="text-center w-full flex justify-evenly items-center h-[600px] relative bg-cover bg-center"
         style={{ backgroundImage: `url(${home})` }}
       >
-        <div className="flex flex-col items-center justify-center gap-5 w-full relative z-10">
-          <h1 className="text-4xl font-light capitalize text-yellow-500">
-            Crafting Stories That Matter
-          </h1>
-          <p className="text-xl text-white font-semilight leading-8">
-            <p>
-              From planning to delivery, we’re here to turn your ideas into
-              impactful productions.
+        <div className="flex justify-between items-center gap-52">
+          <IoIosArrowBack className="w-[50px] h-[50px] text-white" />
+          <div className="flex flex-col items-center justify-center gap-5 w-full relative z-10">
+            <h1 className="text-4xl font-light capitalize text-yellow-500">
+              Crafting Stories That Matter
+            </h1>
+            <p className="text-xl text-white font-semilight leading-8">
+              <p>
+                From planning to delivery, we’re here to turn your ideas into
+                impactful productions.
+              </p>
+              <p>Your journey starts here.</p>
             </p>
-            <p>Your journey starts here.</p>
-          </p>
-          <button className="bg-yellow-500 px-7 py-2 rounded text-base hover:bg-black hover:text-yellow-500 duration-500 transition-all ease-in-out">
-            Explore
-          </button>
+            <button className="bg-yellow-500 px-7 py-2 rounded text-base hover:bg-black hover:text-yellow-500 duration-500 transition-all ease-in-out">
+              Explore
+            </button>
+          </div>
+          <IoIosArrowForward className="w-[50px] h-[50px] text-white" />
         </div>
       </div>
       {/* ABOUT */}
-      <div className="flex items-center justify-center mx-auto relative px-40 py-20 mt-[70px] gap-10">
-        <div className="flex items-center justify-center h-auto">
+      <div className="flex items-center justify-center w-full mx-auto relative px-40 py-20 gap-10">
+        <div className="w-[1000px] absolute left-0 top-[-120px] z-0">
+          <img
+            src={designdot}
+            alt="design background"
+            className="w-full h-auto"
+          />
+        </div>
+        <div className="flex items-center justify-center h-auto z-10">
           <div className="w-full justify-evenly flex items-center">
             <div className="w-[950px]">
               <h1 className="text-4xl font-light py-4">About Us</h1>
@@ -80,10 +93,12 @@ const Home = () => {
         </button>
       </div>
       {/* PORTFOLIO */}
-      <div className="flex justify-center flex-col items-center w-full h-auto px-40 py-20">
+      <div className="flex justify-center relative flex-col items-center w-full h-auto px-40 py-20">
         <h1 className="text-4xl font-light py-4 text-center">Portfolio</h1>
-        <div className="w-full flex justify-between items-center">
-          <div className="w-full py-7 h-auto flex items-center justify-center gap-10 flex-wrap relative">
+        <div className="flex justify-between items-center w-full gap-20 z-10">
+          <IoIosArrowBack className="w-[30px] h-[30px]" />
+
+          <div className="w-full py-7 h-auto flex items-center justify-evenly gap-10 flex-wrap relative">
             {portfolios.map((portfolio) => (
               <div
                 key={portfolio.id}
@@ -97,10 +112,19 @@ const Home = () => {
               </div>
             ))}
           </div>
+
+          <IoIosArrowForward className="w-[30px] h-[30px]" />
         </div>
         <button className="bg-yellow-500 mt-3 px-9 py-2 rounded text-xl hover:shadow-md hover:bg-black hover:text-yellow-500 duration-500 transition-all ease-in-out">
           Explore All
         </button>
+        <div className="w-[1000px] absolute right-0 bottom-0 -z-10 rotate-180">
+          <img
+            src={designdot}
+            alt="design background"
+            className="w-full h-auto"
+          />
+        </div>
       </div>
     </div>
   );
