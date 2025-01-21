6ed2f6450318a760223eaa50b062ce064749aa55
N Mohammed Thoufiq Izhar <thoufiqizhar022@gmail.com>
2025-01-22 01:04:53 +0530

    assets index updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -16,6 +16,20 @@ import digimage3 from './digital-marketing/digital-image3.png'
 import digimage4 from './digital-marketing/digital-image4.png'
 import digimage5 from './digital-marketing/digital-image5.png'
 import digimage6 from './digital-marketing/digital-image6.png'
+// creative production
+import product1 from './productions/product1.png'
+import product2 from './productions/product2.png'
+import product3 from './productions/product3.png'
+import product4 from './productions/product4.png'
+import product5 from './productions/product5.png'
+import product6 from './productions/product6.png'
+// consulting
+import consult1 from './consulting/consult1.png'
+import consult2 from './consulting/consult2.png'
+import consult3 from './consulting/consult3.png'
+import consult4 from './consulting/consult4.png'
+import consult5 from './consulting/consult5.png'
+import consult6 from './consulting/consult6.png'
 
 export {
     home,
@@ -33,5 +47,17 @@ export {
     digimage3,
     digimage4,
     digimage5,
-    digimage6
+    digimage6,
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6,
+    consult1,
+    consult2,
+    consult3,
+    consult4,
+    consult5,
+    consult6
 }
